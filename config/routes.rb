e7c32e9fe68b56a057e71a1c3d06a8e6ace042c1
EC2 Default User <ec2-user@ip-172-31-8-2.ap-northeast-1.compute.internal>
2022-07-27 06:38:57 +0000

    [Fix]コメント欄レイアウト修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'relationships/followings'
+  get 'relationships/followers'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: "homes#top"
@@ -9,9 +11,11 @@ Rails.application.routes.draw do
    resources :book_comments, only: [:create, :destroy]
 
   end
-  resources :users, only: [:index,:show,:edit,:update]
-
-
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
